1d8eff59c3db93b8f400f5eaa7ea740653085313
Ankit181998anand <62587916+Ankit181998anand@users.noreply.github.com>
2023-11-28 18:23:33 +0530

    recycling the things

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -33,71 +33,72 @@ class ProductController extends Controller
 
 
 
-    public function imageUpload(Request $request){
+    // public function imageUpload(Request $request){
 
-        $s3Paths = [];
+    //     $s3Paths = [];
 
-        // Define the files and their respective names
-        $files = [
-            'image1' => 'image1',
-            'image2' => 'image2',
-            'image3' => 'image3'
-        ];
-        $count=0;
-        try {
-            foreach ($files as $requestFile => $fileName) {
-                if ($request->hasFile($requestFile)) {
-                    $count++;
-                    $file = $request->file($requestFile);
-                    $fileName = time() . '_' . $file->getClientOriginalName();
-                    $path = $file->storeAs('images', $fileName, 's3');
-                    $s3Paths[] = 'https://techpack-frontend-test.s3.ap-south-1.amazonaws.com/'.$path;
-                }
-            }
-        } catch (\Exception $e) {
-            \Log::error('S3 Upload Error: ' . $e->getMessage());
-            return response()->json(['error' => 'File upload failed.'], 500);
-        }
+    //     // Define the files and their respective names
+    //     $files = [
+    //         'image1' => 'image1',
+    //         'image2' => 'image2',
+    //         'image3' => 'image3'
+    //     ];
+    //     $count=0;
+    //     try {
+    //         foreach ($files as $requestFile => $fileName) {
+    //             if ($request->hasFile($requestFile)) {
+    //                 $count++;
+    //                 $file = $request->file($requestFile);
+    //                 $fileName = time() . '_' . $file->getClientOriginalName();
+    //                 \Log::info($fileName);
+    //                 $path = $file->storeAs('images', $fileName, 's3');
+    //                 \Log::info($path);
+    //                 $s3Paths[] = 'https://techpack-frontend-test.s3.ap-south-1.amazonaws.com/'.$path;
+    //             }
+    //         }
+    //     } catch (\Exception $e) {
+    //         \Log::error('S3 Upload Error: ' . $e->getMessage());
+    //         return response()->json(['error' => 'File upload failed.'], 500);
+    //     }
 
-        try{
-            foreach($s3Paths as $data){
+    //     // try{
+    //     //     foreach($s3Paths as $data){
 
-                Images::create([
-                    'path'=>$data,
-                    'product_id'=>$request->productId,
-                    'isActive'=>"1"
-                ]);
+    //     //         Images::create([
+    //     //             'path'=>$data,
+    //     //             'product_id'=>$request->productId,
+    //     //             'isActive'=>"1"
+    //     //         ]);
 
-            }
+    //     //     }
         
-        } 
-        catch(\Exception $e){
-            \Log::error('Database Save Error: ' . $e->getMessage());
-            return response()->json(['error' => 'Failed to save to the database.'], 500);
-        }
+    //     // } 
+    //     // catch(\Exception $e){
+    //     //     \Log::error('Database Save Error: ' . $e->getMessage());
+    //     //     return response()->json(['error' => 'Failed to save to the database.'], 500);
+    //     // }
 
-        return response()->json(['paths' => $s3Paths, 'message' => 'S3 paths saved successfully']);
+    //     return response()->json(['paths' => $s3Paths, 'message' => 'S3 paths saved successfully']);
 
-    }
+    // }
 
-    public function fileUpload(Request $request){
-        $file = $request->file('file');
-        $fileName = time() . '_' . $file->getClientOriginalName();
+    // public function fileUpload(Request $request){
+    //     $file = $request->file('file');
+    //     $fileName = time() . '_' . $file->getClientOriginalName();
 
-        try {
-            $path = $file->storeAs('Files', $fileName, 's3');
-        } catch (\Exception $e) {
-            \Log::error('S3 Upload Error: ' . $e->getMessage());
-            return response()->json(['error' => 'File upload failed.'], 500);
-        }
+    //     try {
+    //         $path = $file->storeAs('Files', $fileName, 's3');
+    //     } catch (\Exception $e) {
+    //         \Log::error('S3 Upload Error: ' . $e->getMessage());
+    //         return response()->json(['error' => 'File upload failed.'], 500);
+    //     }
 
-        return response()->json(['path' => 'https://techpack-frontend-test.s3.ap-south-1.amazonaws.com/'.$path], 200);
+    //     return response()->json(['path' => 'https://techpack-frontend-test.s3.ap-south-1.amazonaws.com/'.$path], 200);
 
-    }
+    // }
 
     public function getallProducts(){
-        $productsWithUploads = Products::with('images')
-        ->where('isActive', 1) // Add the where clause to filter by isActive
+        $productsWithUploads = Products::where('isActive', 1) // Add the where clause to filter by isActive
         ->get();
 
         return response()->json(['products' => $productsWithUploads]);
